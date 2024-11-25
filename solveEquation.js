b10435cbc193fee579734575ed37bf877c16db6a
Phoenix <phoenixreid@mail.ru>
2024-11-25 15:30:18 +0300

    First commit

---
+++ solveEquation.js
@@ -0,0 +1,17 @@
+"use strict";
+function solveEquation(a, b, c){
+  let x = [];
+  let D = b*b - 4*a*c;
+  if(D>0){ 
+    x[0] = (-b + Math.sqrt(D)) / (2*a);
+    x[1] = (-b - Math.sqrt(D)) / (2*a);
+  }
+  if(D == 0){
+    x[0] = -b / (2*a);
+  }
+  console.log(D);
+  console.log(x)
+}
+
+solveEquation(1, 2, 1)
+
